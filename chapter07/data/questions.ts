788e9b5f9c54d82ff6b9456290cabb6e13dac447
Kareem Dabbeet <kareemda1@gmail.com>
2024-02-17 19:52:56 +0300

    update questions for chapter 07

--- chapter07/data/questions.ts
+++ chapter07/data/questions.ts
@@ -1,222 +1,181 @@
-export default [
+export const questions: Question[] = [
   {
     id: 1,
-    body: 'which is the tallest animal in the world?',
+    body: 'What is the capital of Russia?',
     answers: [
-      { id: 1, body: 'giraffe' },
-      { id: 2, body: 'lion' },
-      { id: 3, body: 'cheetah' },
-      { id: 4, body: 'bat' }
+      { id: 1, body: 'Moscow' },
+      { id: 2, body: 'Kabul' },
+      { id: 3, body: 'Bogota' },
+      { id: 4, body: 'Brussels' }
     ],
     rightAnswerId: 1
   },
+
   {
     id: 2,
-    body: 'which animal has the longest lifeline?',
+    body: 'What is the capital of Finland?',
     answers: [
-      { id: 1, body: 'falcon' },
-      { id: 2, body: 'the arctic whale' },
-      { id: 3, body: 'octopus' },
-      { id: 4, body: 'elephant' }
+      { id: 1, body: 'Kabul' },
+      { id: 2, body: 'Helsinki' },
+      { id: 3, body: 'Bogota' },
+      { id: 4, body: 'Brussels' }
     ],
     rightAnswerId: 2
   },
+
   {
     id: 3,
-    body: 'how many legs does an octopus have?',
+    body: 'What is the capital of Yemen?',
     answers: [
-      { id: 1, body: 'six' },
-      { id: 2, body: 'ten' },
-      { id: 3, body: 'eight' },
-      { id: 4, body: 'seven' }
+      { id: 1, body: 'Brussels' },
+      { id: 2, body: 'Kabul' },
+      { id: 3, body: "Sana'a" },
+      { id: 4, body: 'Bogota' }
     ],
     rightAnswerId: 3
   },
+
   {
     id: 4,
-    body: 'which bird is the symbol of peace?',
+    body: 'What is the capital of Nigeria?',
     answers: [
-      { id: 1, body: 'dove' },
-      { id: 2, body: 'falcon' },
-      { id: 3, body: 'owl' },
-      { id: 4, body: 'raven' }
+      { id: 1, body: 'Abuja' },
+      { id: 2, body: 'Brussels' },
+      { id: 3, body: 'Bogota' },
+      { id: 4, body: 'Kabul' }
     ],
     rightAnswerId: 1
   },
+
   {
     id: 5,
-    body: 'which is the fastest animal?',
+    body: 'What is the capital of Kenya?',
     answers: [
-      { id: 1, body: 'tiger' },
-      { id: 2, body: 'dog' },
-      { id: 3, body: 'cheetah' },
-      { id: 4, body: 'pig' }
+      { id: 1, body: 'Brussels' },
+      { id: 2, body: 'Nairobi' },
+      { id: 3, body: 'Kabul' },
+      { id: 4, body: 'Bogota' }
     ],
-    rightAnswerId: 3
+    rightAnswerId: 2
   },
+
   {
     id: 6,
-    body: 'which bird lays the largest egg?',
+    body: 'What is the capital of Zimbabwe?',
     answers: [
-      { id: 1, body: 'sparrow' },
-      { id: 2, body: 'eagle' },
-      { id: 3, body: 'woodpecker' },
-      { id: 4, body: 'ostrich' }
+      { id: 1, body: 'Kabul' },
+      { id: 2, body: 'Harare' },
+      { id: 3, body: 'Bogota' },
+      { id: 4, body: 'Brussels' }
     ],
-    rightAnswerId: 4
+    rightAnswerId: 2
   },
+
   {
     id: 7,
-    body: 'which is the only mammal that can fly?',
+    body: 'What is the capital of Colombia?',
     answers: [
-      { id: 1, body: 'goat' },
-      { id: 2, body: 'bat' },
-      { id: 3, body: 'horse' },
-      { id: 4, body: 'donkey' }
+      { id: 1, body: 'Brussels' },
+      { id: 2, body: 'Kabul' },
+      { id: 3, body: 'Bogota' },
+      { id: 4, body: 'Copenhagen' }
     ],
-    rightAnswerId: 2
+    rightAnswerId: 3
   },
+
   {
     id: 8,
-    body: 'which is the only land animal that cannot jump?',
+    body: 'What is the capital of Spain?',
     answers: [
-      { id: 1, body: 'lion' },
-      { id: 2, body: 'goat' },
-      { id: 3, body: 'elephant' },
-      { id: 4, body: 'dog' }
+      { id: 1, body: 'Bogota' },
+      { id: 2, body: 'Kabul' },
+      { id: 3, body: 'Madrid' },
+      { id: 4, body: 'Brussels' }
     ],
     rightAnswerId: 3
   },
+
   {
     id: 9,
-    body: 'which is the only bird that can fly backwards?',
+    body: 'What is the capital of Qatar?',
     answers: [
-      { id: 1, body: 'raven' },
-      { id: 2, body: 'quail' },
-      { id: 3, body: 'hen' },
-      { id: 4, body: 'hummingbirds' }
+      { id: 1, body: 'Brussels' },
+      { id: 2, body: 'Doha' },
+      { id: 3, body: 'Kabul' },
+      { id: 4, body: 'Bogota' }
     ],
-    rightAnswerId: 4
+    rightAnswerId: 2
   },
+
   {
     id: 10,
-    body: 'which bird is known to be the fastest bird?',
+    body: 'What is the capital of Denmark?',
     answers: [
-      { id: 1, body: 'falcon' },
-      { id: 2, body: 'sparrow' },
-      { id: 3, body: 'woodpecker' },
-      { id: 4, body: 'hen' }
+      { id: 1, body: 'Copenhagen' },
+      { id: 2, body: 'Bogota' },
+      { id: 3, body: 'Brussels' },
+      { id: 4, body: 'Kabul' }
     ],
     rightAnswerId: 1
   },
+
   {
     id: 11,
-    body: 'what animal eats mainly bamboo?',
+    body: 'What is the capital of Kenya?',
     answers: [
-      { id: 1, body: 'lion' },
-      { id: 2, body: 'raven' },
-      { id: 3, body: 'dog' },
-      { id: 4, body: 'panda' }
+      { id: 1, body: 'Brussels' },
+      { id: 2, body: 'Nairobi' },
+      { id: 3, body: 'Kabul' },
+      { id: 4, body: 'Bogota' }
     ],
-    rightAnswerId: 4
+    rightAnswerId: 2
   },
+
   {
     id: 12,
-    body: 'which animal can stand on its tail?',
+    body: 'What is the capital of Oman?',
     answers: [
-      { id: 1, body: 'kangaroo' },
-      { id: 2, body: 'goat' },
-      { id: 3, body: 'donkey' },
-      { id: 4, body: 'pig' }
+      { id: 1, body: 'Bogota' },
+      { id: 2, body: 'Kabul' },
+      { id: 3, body: 'Muscat' },
+      { id: 4, body: 'Brussels' }
     ],
-    rightAnswerId: 1
+    rightAnswerId: 3
   },
+
   {
     id: 13,
-    body: 'which mammal is known to have the most powerful bite in the world?',
+    body: 'What is the capital of Qatar?',
     answers: [
-      { id: 1, body: 'lion' },
-      { id: 2, body: 'hippopotamus' },
-      { id: 3, body: 'zebra' },
-      { id: 4, body: 'chimpanzee' }
+      { id: 1, body: 'Brussels' },
+      { id: 2, body: 'Bogota' },
+      { id: 3, body: 'Doha' },
+      { id: 4, body: 'Kabul' }
     ],
-    rightAnswerId: 2
+    rightAnswerId: 3
   },
+
   {
     id: 14,
-    body: 'which animal is known to spend 90% of its day sleeping?',
+    body: 'What is the capital of Morocco?',
     answers: [
-      { id: 1, body: 'rabbit' },
-      { id: 2, body: 'mole' },
-      { id: 3, body: 'koalas' },
-      { id: 4, body: 'camel' }
+      { id: 1, body: 'Rabat' },
+      { id: 2, body: 'Bogota' },
+      { id: 3, body: 'Kabul' },
+      { id: 4, body: 'Brussels' }
     ],
-    rightAnswerId: 3
+    rightAnswerId: 1
   },
+
   {
     id: 15,
-    body: "which animal's poop is known to take the shape of cubes?",
+    body: 'What is the capital of Ukraine?',
     answers: [
-      { id: 1, body: 'wombat' },
-      { id: 2, body: 'gorilla' },
-      { id: 3, body: 'bear' },
-      { id: 4, body: 'wolf' }
-    ],
-    rightAnswerId: 1
-  },
-  {
-    id: 16,
-    body: "what animal is known to be man's best friend?",
-    answers: [
-      { id: 1, body: 'mongoose' },
-      { id: 2, body: 'dog' },
-      { id: 3, body: 'panda' },
-      { id: 4, body: 'elk' }
-    ],
-    rightAnswerId: 2
-  },
-  {
-    id: 17,
-    body: 'how far can a skunk spray?',
-    answers: [
-      { id: 1, body: '7 feet' },
-      { id: 2, body: '5 feet' },
-      { id: 3, body: '3 feet' },
-      { id: 4, body: '10 feet' }
-    ],
-    rightAnswerId: 4
-  },
-  {
-    id: 18,
-    body: 'what animal is covered in quills?',
-    answers: [
-      { id: 1, body: 'porcupine' },
-      { id: 2, body: 'oryx' },
-      { id: 3, body: 'bison' },
-      { id: 4, body: 'alligator' }
-    ],
-    rightAnswerId: 1
-  },
-  {
-    id: 19,
-    body: 'which animal can move its eyes independently?',
-    answers: [
-      { id: 1, body: 'hyena' },
-      { id: 2, body: 'monkey' },
-      { id: 3, body: 'chameleon' },
-      { id: 4, body: 'giraffe' }
+      { id: 1, body: 'Kabul' },
+      { id: 2, body: 'Brussels' },
+      { id: 3, body: 'Kyiv' },
+      { id: 4, body: 'Bogota' }
     ],
     rightAnswerId: 3
-  },
-  {
-    id: 20,
-    body: `what is the name of the largest snake in the world?`,
-    answers: [
-      { id: 1, body: 'eastern tiger snake' },
-      { id: 2, body: 'saw-scaled viper' },
-      { id: 3, body: 'king cobra' },
-      { id: 4, body: 'anaconda' }
-    ],
-    rightAnswerId: 4
   }
-] as Question[]
+]
